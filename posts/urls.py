f095ecdba25caeac213bd1a2660afa03cd70a588
gabeaugusto <gabeaugusto@fetch.ly>
2023-01-16 19:30:08 -0300

    feat: Adds posts slug path

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='posts'),
+    path('<slug:slug>', views.post_detail, name='post-details') #posts/my-first-post
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>POSTS</h1>")
+
+def post_detail(request, slug):
+    return HttpResponse(f"<h1>POST {slug}</h1>")
